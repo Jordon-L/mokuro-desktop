0ff3d889f16241d4cf4f1dcbc21ccdc1c29495d5
Jordon-L <jordonlee1412@gmail.com>
2023-03-29 14:08:13 -0700

    initial

---
+++ mokuro-desktop/src/renderer/src/components/Mokuro.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import SelectDirectory from './SelectDirectory'
+
+export default function Mokuro(): JSX.Element {
+  return (
+    <>
+      <SelectDirectory></SelectDirectory>
+    </>
+  )
+}
